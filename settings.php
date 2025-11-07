c9328bbf5a47bdac7e97fcba23ace4a04f3de786
minhnt minhnt@xdp.vn <minhnt@ozc-company.com>
2025-11-07 09:38:37 +0700

    com

--- settings.php
+++ settings.php
@@ -6,7 +6,14 @@ session_start();
 $host = "localhost";         // because XAMPP runs the server locally
 $user = "root";          // default username for XAMPP's MySQL
 $pwd = "";              // default password is empty in XAMPP
-$sql_db = "your_db_name";  // replace with the actual name of your database
+$sql_db = "exhibition_db";  // replace with the actual name of your database
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
 
